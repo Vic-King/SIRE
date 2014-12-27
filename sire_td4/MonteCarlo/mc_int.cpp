bd03d3552dc6c25a8c445fa7e4a4cb99522ee5f3
Vic-King <victorfrancois@free.fr>
2014-12-27 14:34:50 +0100

    Add sire_td4 MonteCarlo sources

--- sire_td4/MonteCarlo/mc_int.cpp
+++ sire_td4/MonteCarlo/mc_int.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-double int_mc(double(*)(double), double&, double, double, int);
+double int_mc(double(*)(double), double&, double&, double, double, int);
 double f(double); 
 void wait_for_key ();
 
@@ -20,20 +20,25 @@ int main()
     double b = 1.0;               // right endpoint
     int N = 500;
     
-    double varest, mc;
+    double varest_t, varest_e, mc;
     std::vector<double> x, err, var_e, var_t;
 
     for (int n=5; n <= N; n++)
     {
-        mc = int_mc(f, varest, a, b, n);
+        mc = int_mc(f, varest_t, varest_e, a, b, n);
 
         x.push_back(n);
+        var_t.push_back(sqrt(varest_t));
+        var_e.push_back(sqrt(varest_e));
         err.push_back(fabs(mc-1.0));
+
     }
 
     g.reset_plot();
     g.set_grid();
     g.set_style("lines").plot_xy(x,err,"erreur");
+    //g.set_style("lines").plot_xy(x,var_t,"variance analytique");
+    g.set_style("lines").plot_xy(x,var_e,"variance numerique");
 
     wait_for_key();
 
@@ -43,18 +48,46 @@ int main()
 /* 
     Fonction à intégrer 
 */
-    double f(double x)
-{
-    double y = 0.0;
-    return y;
+double f(double x) {
+    return 5.0f * x * x * x * x;
 }    
 
 /* 
     Integration de Monte-Carlo
 */
-double int_mc(double(*f)(double), double& varest, double a, double b, int n)
-{
-    return 0.0;
+double int_mc(double(*f)(double), double& varest_t, double& varest_e, double a, double b, int n) {
+    double resMc = 0.0f;
+    double resVar_t = 0.0f;
+    double resVar_e = 0.0f;
+
+    double resF = 0.0f;
+
+    srand (time(NULL));
+
+    a *= 100.0f;
+    b *= 100.0f;
+
+    for (unsigned int i = 0; i < n; ++i) {
+        double randf = (rand() % (int)(b - a) + (int)a) / 100.0f;
+
+        resF = f(randf);
+
+        resMc += resF;
+
+        // variance analytique :
+        resVar_t += (resF - 1) * (resF - 1);
+
+        // variance numerique :
+        resVar_e += resF * resF;
+    }
+
+    // variance analytique :
+    varest_t = resVar_t / n;
+
+    // variance numerique :
+    varest_e = (1.0f / (n - 1)) * (resVar_e / n - (resMc / n) * (resMc / n));
+
+    return resMc / n;
 }
 
 void wait_for_key ()
